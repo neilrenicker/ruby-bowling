c55f84512954771c3d3daa4bd069d67a28f6f77e
Neil Renicker <neilrenicker@gmail.com>
2013-03-11 15:32:55 -0400

    add all initial business rules

--- bowling.rb
+++ bowling.rb
@@ -2,8 +2,34 @@ require 'pry'
 
 class Bowling
 
-  def turn(roll)
-    true
+  attr_accessor :turns
+
+  def initialize(*turns)
+    @turns = turns
   end
+
+  def turn
+    turns.first
+  end
+
+  def sum
+    turn[0] + turn[1]
+  end
+
+  def is_miss?
+    sum < 10 && sum >= 0
+  end
+
+  def is_spare?
+    sum == 10 && turn.first != 10
+  end
+
+  def is_strike?
+    turn.first == 10
+  end
+
+  # def next_turn
+  #   turns.shift
+  # end
 
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -3,13 +3,39 @@ require './bowling.rb'
 
 describe Bowling do
 
-  it "should be able to create a new game" do
-    game = Bowling.new
+  it "should accept a series of turns" do
+    game = Bowling.new([5,5], [5,5])
+    game.turns.should == [[5,5], [5,5]]
   end
 
-  xit "should recognize 2 rolls within each turn" do
-    game = Bowling.new
-    game.turn([5,5]).should == [5,5]
+  it "should isolate the first turn from a series of turns" do
+    game = Bowling.new([5,5], [3,0])
+    game.turn.should == [5,5]
+  end
+
+  it "should determine when a miss is rolled" do
+    game = Bowling.new([5,3], [3,0])
+    game.is_miss?.should be_true
+  end
+
+  it "should determine when a spare is rolled" do
+    game = Bowling.new([5,5], [3,0])
+    game.is_spare?.should be_true
+  end
+
+  it "should determine when a strike is rolled" do
+    game = Bowling.new([10,0], [3,0])
+    game.is_strike?.should be_true
+  end
+
+  it "should not determine a spare if a strike is rolled" do
+    game = Bowling.new([10,0], [3,0])
+    game.is_spare?.should be_false
+  end
+
+  it "should not determine a spare when a miss is rolled" do
+    game = Bowling.new([5,3], [3,0])
+    game.is_spare?.should be_false
   end
 
 end
